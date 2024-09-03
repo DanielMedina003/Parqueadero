034d33ed23b66cf8691d617b858368b4498a361d
DanielMedina003 <162385548+DanielMedina003@users.noreply.github.com>
2024-09-02 22:24:02 -0500

    Registro_Parqeuadero

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de sesion</title>
+    <link rel="stylesheet" href="Style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" 
+    integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" /> 
+</head>
+<body>
+    <form action="IniciarSesion.psp" method="POST">
+        <h1>BIENVENIDO</h1>
+        <i class="fa-solid fa-user"></i>
+        <label>Usuario</label>
+        <input type="text" name="Usuario" placeholder="Nombre de usuario">
+
+        <i class="fa-solid fa-unlock"></i>
+        <label>Matricula</label>
+        <input type="text" name="Matricula" placeholder="No_matricula">
+
+        <i class="fa-solid fa-user"></i>
+        <label>Hora de  Ingreso</label>
+        <input type="text" name="Hora_de_Ingreso" placeholder="Hora">
+
+        <button type="submit">Registrarse</button>
+
+    </form>
+    
+</body>
+</html>
